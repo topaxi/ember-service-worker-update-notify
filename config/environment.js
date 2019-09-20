50b88428fcde80511f80921a464d441e9e30ee45
Jonas Cosandey <jonas.cosandey@adfinis-sygroup.ch>
2019-09-20 15:52:02 +0200

    Make pollInterval configurable

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  return { };
+  return {
+    'ember-service-worker-update-notify': {
+      pollingInterval:1200000 // 20 min in ms
+    }
+  };
 };
