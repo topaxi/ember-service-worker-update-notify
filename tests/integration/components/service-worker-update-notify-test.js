84a5390865a83bacda80b7e1846cc38e2722ebbf
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-09-25 13:49:55 -0700

    cleanup. convert tests to new style

--- tests/integration/components/service-worker-update-notify-test.js
+++ tests/integration/components/service-worker-update-notify-test.js
@@ -1,38 +1,36 @@
-import { moduleForComponent, skip } from 'ember-qunit'
+import { module, skip } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile'
 
-moduleForComponent(
-  'service-worker-update-notify',
-  'Integration | Component | service worker update notify',
-  {
-    integration: true,
-  },
-)
+module('Integration | Component | service worker update notify', function(hooks) {
+  setupRenderingTest(hooks);
 
-skip('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  skip('it renders', function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{service-worker-update-notify}}`)
+    this.render(hbs`{{service-worker-update-notify}}`)
 
-  assert.equal(
-    this.$()
-      .text()
-      .trim(),
-    '',
-  )
+    assert.equal(
+      this.$()
+        .text()
+        .trim(),
+      '',
+    )
 
-  // Template block usage:
-  this.render(hbs`
-    {{#service-worker-update-notify}}
-      template block text
-    {{/service-worker-update-notify}}
-  `)
+    // Template block usage:
+    this.render(hbs`
+      {{#service-worker-update-notify}}
+        template block text
+      {{/service-worker-update-notify}}
+    `)
 
-  assert.equal(
-    this.$()
-      .text()
-      .trim(),
-    'template block text',
-  )
-})
+    assert.equal(
+      this.$()
+        .text()
+        .trim(),
+      'template block text',
+    )
+  })
+});
--- tests/unit/utils/service-worker-has-update-test.js
+++ tests/unit/utils/service-worker-has-update-test.js
@@ -1,10 +1,10 @@
 import serviceWorkerHasUpdate from 'dummy/utils/service-worker-has-update';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | service worker has update');
-
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = serviceWorkerHasUpdate();
-  assert.ok(result);
+module('Unit | Utility | service worker has update', function() {
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = serviceWorkerHasUpdate();
+    assert.ok(result);
+  });
 });
