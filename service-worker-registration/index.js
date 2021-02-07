a2910a0ccf883d844c76d98da8180325b311449b
Sinan Keskin <sinan@kesk.in>
2021-02-07 19:00:31 +0300

    Fix after first time register notify

--- service-worker-registration/index.js
+++ service-worker-registration/index.js
@@ -1,30 +1,33 @@
 import { addSuccessHandler } from 'ember-service-worker/service-worker-registration'
 
 function hasServiceWorkerUpdate(resolve) {
-  addSuccessHandler(function(reg) {
-    reg.onupdatefound = function() {
-      const { installing } = reg;
+  addSuccessHandler(function (reg) {
+    reg.onupdatefound = function () {
+      const { installing } = reg
 
-      installing.onstatechange = function() {
+      installing.onstatechange = function () {
         if (installing.state === 'activated') {
-          resolve(navigator.serviceWorker.controller !== null)
+          if (localStorage.getItem('sw_registered') === 'ok') {
+            resolve(navigator.serviceWorker.controller !== null)
+          } else {
+            localStorage.setItem('sw_registered', 'ok')
+            resolve(false)
+          }
         }
       }
     }
-  });
+  })
 }
 
 // https://caniuse.com/#search=Promise
 // IE11 (2.5%) and Opera Mini (2.3%) do not support Promises
-const arePromisesSupported = 'Promise' in window;
-const disablePolyfill = ({
-  then: function() {
-    return false;
-  }
-});
+const arePromisesSupported = 'Promise' in window
+const disablePolyfill = {
+  then: function () {
+    return false
+  },
+}
 
-window.hasServiceWorkerUpdate =
-  arePromisesSupported
+window.hasServiceWorkerUpdate = arePromisesSupported
   ? new Promise(hasServiceWorkerUpdate)
   : disablePolyfill
-
